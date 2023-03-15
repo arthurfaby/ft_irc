f500e3a9e425c97e63ed9f44bce3ce07ca7ff07b
Arthur Faby <afaby@3F2.42angouleme.fr>
2023-03-15 17:27:38 +0100

    Fill Client.hpp file

--- srcs/Client/Client.hpp
+++ srcs/Client/Client.hpp
@@ -1,20 +1,48 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include "Server.hpp"
+#include "Channel.hpp"
+
 class Client
 {
 
-private:
-
-	//private attributes here;
-
 public:
 
+	// Constructors
 	Client(void);
 	Client(const Client & other);
 	~Client(void);
 	Client & operator=(const Client & other);
 
+	// Setters
+	void			setName(const std::string& name);
+	void			setChannel(const Channel& channel);
+	void			setOp(const bool& is_op);
+
+	// Getters
+	std::string		getname(void) const;
+	const Server&	getIntroducer(void) const;
+	const int		getSockfd(void) const;
+	Channel*		getChannel(void) const;
+	bool			isOp(void) const;
+	bool			isInChannel(void) const;
+
+	// Methods
+	bool			joinChannel(Channel* channel);
+	bool			sendMessageToClient(const Client& other);
+	bool			sendMessageToChannel(const Channel& other);
+	bool			quitChannel(Channel* channel);
+
+private:
+
+	std::string				_name;			// Unique nickname of the client (max 9 characters)
+	const Server&			_introducer;	// Server which introduced the client
+	const int				_sockfd;		// fd which referred to the client socket
+	std::vector<Channel*>	_channels;		// Channel in which the client is (or NULL if not)
+	bool					_is_op;			// true if client is an operator, false otherwise
+
+
 };
 
-#endif
+#endif /* !CLIENT_HPP */
