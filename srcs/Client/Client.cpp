61b235433caca7dd89eac265a3eecfd2a47e06b7
Pierre Rabaud <pirabaud@3F1.42angouleme.fr>
2023-03-16 18:03:59 +0100

    add .swp gitiginore

--- srcs/Client/Client.cpp
+++ srcs/Client/Client.cpp
@@ -1,26 +1,20 @@
 #include "Client.hpp"
-#include <iostream>
 
 Client::Client(void)
 {
-	std::cout << "Default constructor called" << std::endl;
+	return ;//exception ?	
 }
+
+Client::Client(const Server& introducer, const int& sockfd)
+{}
 
 Client::Client(const Client & other)
-{
-	(void)other;
-	std::cout << "Default copy constructor called" << std::endl;
-}
+{}
 
 Client::~Client(void)
-{
-	std::cout << "Default destructor called" << std::endl;
-}
+{}
 
 Client & Client::operator=(const Client & other)
 {
-	//copy operations here;
-	(void)other;
-	std::cout << "Assignment operator called" << std::endl;
 	return (*this);
 }
