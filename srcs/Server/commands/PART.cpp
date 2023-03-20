85f28217d1e9145006bc6f9d5d6b173de278a143
Arthur Faby <afaby@3E1.42angouleme.fr>
2023-03-20 15:56:27 +0100

    PART command done

--- srcs/Server/commands/PART.cpp
+++ srcs/Server/commands/PART.cpp
@@ -1,12 +1,51 @@
 #include "Server.hpp"
 
-void	Server:_PART(Client* client, std::vector<std::string>& args)
+static bool	isInVector(const std::string& name, const std::vector<Channel*>& channels)
+{
+	for  (size_t i = 0; i < channels.size(); ++i)
+		if (name == channels[i]->getName())
+			return (true);
+	return (false);
+}
+
+void	Server::_PART(Client* client, std::vector<std::string>& args)
 {
 	if (args.size() < 3) return;
 
 	std::vector<std::string>	copy(args);
 	std::vector<std::string>	channels;
-	size_t						pos(0);
+	Channel						*actual;
+	size_t						pos;
 
-	std::cout << args << std::endl;
+	pos = copy[1].find(',');
+	while (pos != std::string::npos)
+	{
+		channels.push_back(copy[1].substr(0, pos));
+		copy[1].erase(0, pos + 1);
+		pos = copy[1].find(',');
+	}
+	channels.push_back(copy[1]);
+
+	for (size_t i = 0; i < channels.size(); ++i)
+	{
+		if (channels[i][0] != '#')
+		{
+			this->sendMessage(client, "Channel '" + channels[i] + "' is invalid.\n");
+			continue;
+		}
+		if (!isInVector(channels[i], _channels))
+		{
+			this->sendMessage(client, "Channel '" + channels[i] + "' does not exists.\n");
+			continue;
+		}
+		else
+		{
+			for (size_t j = 0; j < _channels.size(); ++j)
+				if (channels[i] == _channels[j]->getName())
+					actual = _channels[j];
+			for (size_t j = 0; j < actual->getMembers().size(); ++j)
+				this->sendMessage(actual->getMembers()[j], channels[i] + ": " + client->getName() + " has leaved the channel (" + args[3] + ").\n");
+			actual->removeMember(client);
+		}
+	}
 }
